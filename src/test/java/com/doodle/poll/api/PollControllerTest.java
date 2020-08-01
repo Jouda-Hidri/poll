1c37e6a0385998f67fa120c03991e0b6b601d646
Jouda Hidri <hidrijouda@gmail.com>
2020-08-01 21:10:40 +0200

    Implement test for controller

--- src/test/java/com/doodle/poll/api/PollControllerTest.java
+++ src/test/java/com/doodle/poll/api/PollControllerTest.java
@@ -1,14 +1,49 @@
 package com.doodle.poll.api;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class PollControllerTest {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testFindByUser() throws Exception {
+		mockMvc.perform(get("/poll").param("user", "mh+sample@doodle.com")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testFindByTitle() throws Exception {
+		mockMvc.perform(get("/poll").param("title", "Qui sont les superhéros Marvel les plus oufs?"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testFindByDate() throws Exception {
+		mockMvc.perform(get("/poll").param("afterDate", "2020-07-01 16:13:15.0")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void testBadRequest() throws Exception {
+		mockMvc.perform(get("/poll").param("user", "mh+sample@doodle.com").param("afterDate", "2020-07-01 16:13:15.0"))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void testBadRequestWithoutParams() throws Exception {
+		mockMvc.perform(get("/poll")).andExpect(status().isBadRequest());
 	}
 
 }
