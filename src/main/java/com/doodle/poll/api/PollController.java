1a2643ff3eff0a3fdb251402b4e970ed7065d49e
Jouda Hidri <hidrijouda@gmail.com>
2020-08-02 15:27:52 +0200

    Implement openapi

--- src/main/java/com/doodle/poll/api/PollController.java
+++ src/main/java/com/doodle/poll/api/PollController.java
@@ -12,15 +12,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.doodle.poll.api.dto.PollDto;
 import com.doodle.poll.service.PollService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class PollController {
 
 	@Autowired
 	private PollService service;
 
+	@Operation(summary = "List all polls by title or created by a given  user or after a given date")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the polls", content = {
+					@Content(mediaType = "application/json") }),
+			@ApiResponse(responseCode = "400", description = "Invalid filter supplied", content = @Content) })
 	@GetMapping("/poll")
-	public ResponseEntity<List<PollDto>> findBy(@RequestParam(required = false) String user,
-			@RequestParam(required = false) String title, @RequestParam(required = false) Timestamp afterDate) {
+	public ResponseEntity<List<PollDto>> findBy(
+			@Parameter(description = "Email of user who created the poll ex. mh+sample@doodle.com") @RequestParam(required = false) String user,
+			@Parameter(description = "Title of poll ex. Marvel") @RequestParam(required = false) String title,
+			@Parameter(description = "Date after which poll was created ex. 2020-08-02 09:13:15") @RequestParam(required = false) Timestamp afterDate) {
 
 		if (user != null && title == null && afterDate == null) {
 			return ResponseEntity.ok().body(service.findByUser(user));
